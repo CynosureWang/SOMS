ecb661add88c7b9d5474ed2cfac1b800fc482514
Cynosure.Wang <cynosure@mfnit.com>
2026-09-17 12:07:21 +0800

    SOMS更新日志  -新增Product库存服务模块  -Product库存服务初始化

---
+++ soms-server/soms-product-service/src/main/java/com/mfnit/product/ProductApplication.java
@@ -0,0 +1,21 @@
+package com.mfnit.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Project SOMS
+ * @Author Cynosure.Wang
+ * @Version 1.0.0
+ * @CreateTime 2026/9/17 12:04
+ * @Description SOMS Product Service
+ * @Copyright Copyright © 2026 Zaozhuang Memorial Future Network Information Technology Co., Ltd. All rights reserved
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
